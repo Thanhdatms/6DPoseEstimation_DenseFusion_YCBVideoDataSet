4d82bff4247db521c537c4111fde36aee887f227
Thanhdatms <dangthanhdatuit@gmail.com>
2025-11-11 23:15:00 +0700

    feat: create dataset YCB

--- src/vanilla_segmentation/dataset.py
+++ src/vanilla_segmentation/dataset.py
@@ -9,8 +9,6 @@ import torch
 import torch.utils.data as data
 
 
-
-
 class SegmentationDataSet(data.Dataset):
     def __init__(self, root_dir, txtlist, use_noise=False, transform=None):
         """
@@ -21,22 +19,27 @@ class SegmentationDataSet(data.Dataset):
             transform: 
         """
         self.root_dir = root_dir
-        self.txtlist = txtlist
         self.use_noise = use_noise
         self.transform = transform
 
-        # read file path
-        with open(txtlist, 'r') as f:
-            self.path = [line.strip() for line in f if line.strip()]
-
-        self.real_path = [p for p in self.path if p.startswith('data/')]
-        self.len_data = len(self.path)
-        self.back_len = len(self.real_path)
-
+        # define variables
         
 
+        # read file path
+        with open(txtlist, 'r') as f:
+            self.path = [line.strip() for line in f if line.strip()] # including real and synthetic
+
+        self.real_path = [p for p in self.path if p.startswith('data/')] # only real data not synthetic
+        self.data_len = len(self.path)
+        self.back_len = len(self.real_path)
+
         # Augmentation
-        self.color_jiter = transforms.ColorJitter(0.2, 0.2, 0.2, 0.05)
+        self.trancolor = transforms.ColorJitter(0.2, 0.2, 0.2, 0.05)
         self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                               std=[0.229, 0.224, 0.225])
-
+    
+    def __getitem__(self, idx):
+        index = random
+        
+    def __len__(self):
+        return self.data_len
