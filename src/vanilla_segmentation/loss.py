ce467703e0c23b5647d00491a5c7c37973d26b8d
Thanhdatms <dangthanhdatuit@gmail.com>
2025-11-22 01:58:31 +0700

    feat: create loss function

---
+++ src/vanilla_segmentation/loss.py
@@ -0,0 +1,29 @@
+from torch.nn.modules.loss import _Loss
+from torch.autograd import Variable
+import torch
+import torch.nn as nn
+import random
+import copy
+import math
+
+CEloss = nn.CrossEntropyLoss()
+
+def loss_calculation (semantic, target):
+
+    # sematic has shape [batch_sizem, 22, 480, 640]
+    pixel_num = semantic.size()[3] * semantic.size()[2]
+    batch_size = semantic.size()[0]
+
+    target = target.view(batch_size, -1).view(-1).contiguous() # flatten to [batch_size * 480 * 640]
+    semantic = semantic.view(batch_size, 3, pixel_num).permute(0,2,1).contiguous().view(batch_size * pixel_num, 3).contiguous()
+
+    semantic_loss = CEloss(semantic, target)
+
+    return semantic_loss
+
+class Loss(_Loss):
+    def __init__(self):
+        super(Loss, self).__init__()
+
+    def forward(self, semantic, target):
+        loss_calculation(semantic, target)
