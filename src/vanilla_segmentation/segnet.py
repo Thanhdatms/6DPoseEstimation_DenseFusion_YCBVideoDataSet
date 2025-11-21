248381d299fdf72fe1db26a1cee50e0f4e523335
Thanhdatms <dangthanhdatuit@gmail.com>
2025-11-21 23:41:56 +0700

    feat: create segnet layer

---
+++ src/vanilla_segmentation/segnet.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import OrderedDict
+
+class SegNet(nn.Module):
+    def __init__(self, input_nbr=3, label_nbr=22):
+        super(SegNet, self).__init__()
+
+        batchrorm_momentum = 0.1
+
+        self.conv11 = nn.Conv2d(input_nbr, 64, kernel_size=3, padding=1)
+        self.bn11 = nn.BatchNorm2d(64, momentum=batchrorm_momentum)
+        self.conv12 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.bn12 = nn.BatchNorm2d(64, momentum=batchrorm_momentum)
+
+        self.conv21 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn21 = nn.BatchNorm2d(128, momentum=batchrorm_momentum)
+        self.conv22 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.bn22 = nn.BatchNorm2d(128, momentum=batchrorm_momentum)
+
+        self.conv31 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.bn31 = nn.BatchNorm2d(256, momentum= batchrorm_momentum)
+        self.conv32 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.bn32 = nn.BatchNorm2d(256, momentum= batchrorm_momentum)
+        self.conv33 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.bn33 = nn.BatchNorm2d(256, momentum= batchrorm_momentum)
+
+        self.conv41 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
+        self.bn41 = nn.BatchNorm2d(512, momentum= batchrorm_momentum)
+        self.conv42 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn42 = nn.BatchNorm2d(512, momentum= batchrorm_momentum)
+        self.conv43 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn43 = nn.BatchNorm2d(512, momentum= batchrorm_momentum)
+
+        self.conv51 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn51 = nn.BatchNorm2d(512, momentum= batchrorm_momentum)
+        self.conv52 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn52 = nn.BatchNorm2d(512, momentum= batchrorm_momentum)
+        self.conv53 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn53 = nn.BatchNorm2d(512, momentum= batchrorm_momentum)
+
+        self.conv53d = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn53d = nn.BatchNorm2d(512, momentum= batchrorm_momentum)
+        self.conv52d = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn52d = nn.BatchNorm2d(512, momentum= batchrorm_momentum)
+        self.conv51d = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn51d = nn.BatchNorm2d(512, momentum= batchrorm_momentum)
+
+        self.conv43d = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn43d = nn.BatchNorm2d(512, momentum= batchrorm_momentum)
+        self.conv42d = nn.Conv2d(512, 512, kernel_size=3, padding=1)
+        self.bn42d = nn.BatchNorm2d(512, momentum= batchrorm_momentum)
+        self.conv41d = nn.Conv2d(512, 256, kernel_size=3, padding=1)
+        self.bn41d = nn.BatchNorm2d(256, momentum= batchrorm_momentum)
+
+        self.conv33d = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.bn33d = nn.BatchNorm2d(256, momentum= batchrorm_momentum)
+        self.conv32d = nn.Conv2d(256, 256, kernel_size=3, padding=1)
+        self.bn32d = nn.BatchNorm2d(256, momentum= batchrorm_momentum)
+        self.conv31d = nn.Conv2d(256,  128, kernel_size=3, padding=1)
+        self.bn31d = nn.BatchNorm2d(128, momentum= batchrorm_momentum)
+
+        self.conv22d = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.bn22d = nn.BatchNorm2d(128, momentum= batchrorm_momentum)
+        self.conv21d = nn.Conv2d(128, 64, kernel_size=3, padding=1)
+        self.bn21d = nn.BatchNorm2d(64, momentum= batchrorm_momentum)
+
+        self.conv12d = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.bn12d = nn.BatchNorm2d(64, momentum= batchrorm_momentum)
+        self.conv11d = nn.Conv2d(64, label_nbr, kernel_size=3, padding=1)
+
+
+
+
+
